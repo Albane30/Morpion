d0e2d26ff083dee774110b6f3a2b229907967e90
cleroux <cleroux84@gmail.com>
2020-06-04 11:16:20 +0200

    morpion fonctionnel

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+/* Grille de jeu */
+
 document.querySelector(".container").innerHTML = 
 
        `
@@ -21,8 +23,8 @@ document.querySelector(".container").innerHTML =
             </div>
             `
 
-let xGame = []
-let oGame = []
+let xGame = []/*  array des coups joués avec croix */
+let oGame = []   /* array des coups joués avec ronds */
 let button = document.querySelector(".buttonPlay")
 let player1 = document.querySelector("#player1")
 let player2 = document.querySelector("#player2")
@@ -34,24 +36,23 @@ button.addEventListener("click", function(){
 })
 
 let squares = document.querySelectorAll(".square")
-squares.forEach(square => square.addEventListener("click",function(e){
+squares.forEach(square => square.addEventListener("click",function(e)
+{
    
- if(e.target.classList.contains("player") && actualPlayer === player1){
+ if(e.target.classList.contains("player") && actualPlayer === player1)
+ {
      e.target.classList.replace("player","playerX")
      xGame.push(square["id"])
      console.log(xGame.sort())
      actualPlayer = player2
      player1.style.backgroundColor = "";
      player2.style.backgroundColor = "yellow";
-    }
-    /* else {
-        e.target.classList.remove("player")
-       } */
+}
 
   if(e.target.classList.contains("player") && actualPlayer === player2){
         e.target.classList.replace("player","playerO")
         oGame.push(square["id"])
-        console.log(oGame.sort())
+         console.log(oGame.sort()) 
         actualPlayer = player1
         player2.style.backgroundColor = "";
         player1.style.backgroundColor = "yellow";
@@ -65,64 +66,86 @@ squares.forEach(square => square.addEventListener("click",function(e){
           
           
           let squareClass = square.classList[1]
-/*      / console.log(squareId)
-          console.log(squareClass)  */
- 
 
-function win()
+
+ function win()
     {
-        let win1 = [squares["0"]["id"], squares["1"]["id"], squares["2"]["id"]]
-        let win2 = [squares["3"]["id"], squares["4"]["id"], squares["5"]["id"]]
-        let win3 = [squares["6"]["id"], squares["7"]["id"], squares["8"]["id"]]
-        let win4 = [squares["0"]["id"], squares["3"]["id"], squares["6"]["id"]]
-        let win5 = [squares["1"]["id"], squares["4"]["id"], squares["7"]["id"]]
-        let win6 = [squares["2"]["id"], squares["5"]["id"], squares["8"]["id"]]
-        let win7 = [squares["0"]["id"], squares["4"]["id"], squares["8"]["id"]]
-        let win8 = [squares["2"]["id"], squares["4"]["id"], squares["6"]["id"]]
-
-         /* console.log(win1)  */ 
-        if(xGame [["0"], ["1"], ["2"]] == win1[["0"], ["1"], ["2"]]
-        || xGame[["0"], ["1"], ["2"]] == win2[["0"], ["1"], ["2"]]
-        || xGame[["0"], ["1"], ["2"]] == win3[["0"], ["1"], ["2"]]
-        || xGame[["0"], ["1"], ["2"]] == win4[["0"], ["1"], ["2"]]
-        || xGame[["0"], ["1"], ["2"]] == win5[["0"], ["1"], ["2"]]
-        || xGame[["0"], ["1"], ["2"]] == win6[["0"], ["1"], ["2"]]
-        || xGame[["0"], ["1"], ["2"]] == win7[["0"], ["1"], ["2"]]
-        || xGame[["0"], ["1"], ["2"]] == win8[["0"], ["1"], ["2"]]
-        )
-
-        {
-            
-            alert("Player 1 win")
+ /* win X   */  
+if(xGame.includes("1") && xGame.includes("2") && xGame.includes("3"))
+        {            
+            alert("Player1  win")
         }
+if(xGame.includes("4") && xGame.includes("5") && xGame.includes("6"))
+        {
+            alert("Player1  win")
+        }
+if(xGame.includes("7") && xGame.includes("8") && xGame.includes("9"))
+        {
+            alert("Player1  win")
+        }
+if(xGame.includes("1") && xGame.includes("4") && xGame.includes("7"))
+        {
+            alert("Player1  win")
+        }
+if(xGame.includes("2") && xGame.includes("5") && xGame.includes("8"))
+        {
+            alert("Player1  win")
+        }
+if(xGame.includes("3") && xGame.includes("6") && xGame.includes("9"))
+        {
+            alert("Player1  win")
+        }
+if(xGame.includes("1") && xGame.includes("5") && xGame.includes("9"))
+        {
+            alert("Player1  win")
+        }
+if(xGame.includes("3") && xGame.includes("5") && xGame.includes("7"))
+        {
+            alert("Player1  win")
+        }
+
+
+       /*  win O */
+       if(oGame.includes("1") && oGame.includes("2") && oGame.includes("3"))
+        {            
+            alert("Player2  win")
+        }
+if(oGame.includes("4") && oGame.includes("5") && oGame.includes("6"))
+        {
+            alert("Player2  win")
+        }
+if(oGame.includes("7") && oGame.includes("8") && oGame.includes("9"))
+        {
+            alert("Player2  win")
+        }
+if(oGame.includes("1") && oGame.includes("4") && oGame.includes("7"))
+        {
+            alert("Player2  win")
+        }
+if(oGame.includes("2") && oGame.includes("5") && oGame.includes("8"))
+        {
+            alert("Player2  win")
+        }
+if(oGame.includes("3") && oGame.includes("6") && oGame.includes("9"))
+        {
+            alert("Player2  win")
+        }
+if(oGame.includes("1") && oGame.includes("5") && oGame.includes("9"))
+        {
+            alert("Player2  win")
+        }
+if(oGame.includes("3") && oGame.includes("5") && oGame.includes("7"))
+        {
+            alert("Player2  win")
+        }
+
     } 
- win() 
-  
+ win()  
 }))
 
    
 
 
-/* console.log(squares)  */
-/* squares.forEach(square => square.addEventListener("click",function(){
-
-    const playerActuel = "X"
-    square.innerHTML = playerActuel
-    let squareId = square["id"]
-    console.log(squareId)
-    let squareDom = document.querySelector(`#${square["id"]}`)
-    console.log(squareDom) */
-    
-  
-
-
-
-
-/* squares.addEventListener("click",function(event){
-    let squareId = document.querySelector(`#${square["id"]}`)
-    event.target.squareID
-    console.log(squareID)
-}) */
 
  function Morpion()
 {
@@ -131,12 +154,3 @@ function win()
     let O = "O"
 
 }  
-/* 
-fonction : joueur choisi pion
-fonction  : si croix choisie - le second joueur aura le Rond
-fonction : si croix jouée rond obligé et inversement
-fonction : si case jouée : bloquée ou alert
-fonction : si cases qui se touchent ont même symbole : gagné */
-
-/* Donner la croix ou le rond en fonction de la parite du coup */
-
